9cefbd33f54be1ef8a8d0ff05809384807c621ee
Yohny Ospina <Yohny.Ospina@teaminternational.com>
2021-10-30 21:07:19 -0500

    'cambio'

---
+++ Dtos/CupboardDto/ReadingCupboardDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiProductManagment.Dtos.CupboardDto
+{
+    public class ReadingCupboardDto
+    {
+        public int Identification { get; set; }
+        public int Product { get; set; }
+        public string Section { get; set; }
+        public int Inputs { get; set; }
+        public int Outputs { get; set; }
+    }
+}
